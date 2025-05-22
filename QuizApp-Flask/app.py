bcff5274b1df43e69bb94652230ad91b40f60707
ZainKhan <73061762+Zain-art@users.noreply.github.com>
2025-05-22 13:21:13 +0500

    update quizapp-flask

--- QuizApp-Flask/app.py
+++ QuizApp-Flask/app.py
@@ -8,4 +8,5 @@ def load_user(user_id):
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    print("Starting Flask server...")
+    app.run(host="0.0.0.0", port=5000)
--- QuizApp-Flask/create_app.py
+++ QuizApp-Flask/create_app.py
@@ -1,33 +1,26 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from quiz_app.routes import main
-from quiz_app.extentions import db  # db = SQLAlchemy() defined here
+from quiz_app.extentions import db
+from flask_login import LoginManager
 
 login_manager = LoginManager()
 
-def create_app(config_file: str = "config.py") -> Flask:
-    """
-    Create flask instance, set configuration from config_file.
-
-    Parameters:
+def create_app(config_file:str="config.py") -> Flask:
+    '''Create flask instance, set configuration from config_file.
+    Paramaneter:
         - config_file: str
-    Returns:
+    Return:
         - app: Flask
-    """
+    '''
     app = Flask(__name__)
+
     app.config["UPLOAD_FOLDER"] = "upload_files"
     app.config.from_pyfile(config_file)
-
-    # Initialize extensions with app
+    app.register_blueprint(main)
     db.init_app(app)
+
     login_manager.init_app(app)
     login_manager.login_view = "main.login"
-
-    # Register blueprints
-    app.register_blueprint(main)
-
-    # Create tables
     with app.app_context():
         db.create_all()
 
