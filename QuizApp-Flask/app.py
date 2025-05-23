2e600beafaf33fd03feb8887dcaa38c5bf1e1c28
ZainKhan <73061762+Zain-art@users.noreply.github.com>
2025-05-23 11:04:49 +0500

    update app.py file in flask

--- QuizApp-Flask/app.py
+++ QuizApp-Flask/app.py
@@ -8,5 +8,5 @@ def load_user(user_id):
 app = create_app()
 
 if __name__ == "__main__":
-    print("Starting Flask server... this is main app file")
+    print("Starting Flask server... this is main ")
     app.run(host="0.0.0.0", port=5000)
